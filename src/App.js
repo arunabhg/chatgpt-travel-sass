0a33c5ff57599c55a2c4a8a86763814ece1be189
Arunabh Ghosh <arunabh.tech@gmail.com>
2023-02-14 21:58:36 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [message, setMessage] = useState("");
+	const [response, setResponse] = useState("");
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		fetch("http://localhost:3001/", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({ message })
+		})
+			.then((res) => res.json())
+			.then((data) => setResponse(data.message));
+	};
+
+	return (
+		<div className="App">
+			<form onSubmit={handleSubmit}>
+				<textarea
+					value={message}
+					onChange={(e) => setMessage(e.target.value)}></textarea>
+				<button type="submit">Submit</button>
+			</form>
+			<div>{response}</div>
+		</div>
+	);
 }
 
 export default App;
